c2e4e1fc9e412cca3c222789cbb40976ecdbbcb1
QuickRecon <aren.leishman@gmail.com>
2015-12-08 13:01:26 +0800

    old code I found

---
+++ socket test/socket.h
@@ -0,0 +1,9 @@
+
+
+#include<iostream>    //cout
+#include<stdio.h> //printf
+#include<string.h>    //strlen
+#include<string>  //string
+#include<sys/socket.h>    //socket
+#include<arpa/inet.h> //inet_addr
+#include<netdb.h> //hostent
